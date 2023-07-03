91eb1ec2b012303a0732b207f5c02da991071951
Andres Lobaton <andrespipe021028@gmail.com>
2023-07-02 21:42:41 -0500

    Update list.php
    uniqe id

--- app/views/budgets/list.php
+++ app/views/budgets/list.php
@@ -4,8 +4,8 @@ $fillable = ["id_temporal_budget", "id_temporal_budget", "name", "total_amount",
 
 $configTable = [
     "use" => "custom",
-    "html" => '<form method="POST" action="' . route("budget/execList/:id_temporal_budget") . '" id="form_save" />
-                <button class="dropdown-item" role="button" type="submit" form="form_save"> Ejecutar presupuesto </button>
+    "html" => '<form method="POST" action="' . route("budget/execList/:id_temporal_budget") . '" id="form_save_:id_temporal_budget" />
+                <button class="dropdown-item" role="button" type="submit" form="form_save_:id_temporal_budget"> Ejecutar presupuesto </button>
                 <a class="dropdown-item"  href="' . route("budget/list_outflows/:id_temporal_budget") . '"> Listar presupuesto </a>
                 <a id="edit-modal-budget__me" onclick="editTemporalBudget(event)" data-name=":name" data-id=":id_temporal_budget" data-target="#myModal" href="#" type="button" data-toggle="modal" class="dropdown-item" "> Editar </a>
                 <a class="dropdown-item"  href="' . route("budget/delete/:id_temporal_budget") . '"> Eliminar </a>
