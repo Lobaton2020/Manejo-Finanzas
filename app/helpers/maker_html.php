aeabf8fa3e8ba4a6f7b566b407642cbf59db90e5
Andres Lobaton <andrespipe021028@gmail.com>
2023-12-18 19:34:36 -0500

    feat: calculate common percentage

--- app/helpers/maker_html.php
+++ app/helpers/maker_html.php
@@ -354,6 +354,9 @@ function structure_html_send_email($data)
 
 function card_statistic_component($title, $amount1, $amount2, $amount3 = [])
 {
+    $total = count($amount3) > 0 ? $amount1[1] + $amount3[1] : $amount1[1] + $amount2[1];
+    $part = count($amount3) > 0 ? $amount3[1] : $amount2[1];
+    $percentage = intval($part * 100 / $total);
     return ' <div class="col-sm-6 col-xl-4">
     <div class="card">
         <div class="card-heading p-4">
@@ -366,8 +369,9 @@ function card_statistic_component($title, $amount1, $amount2, $amount3 = [])
             </div>
             <h6 class="mt-4">
                 <span data-toggle="tooltip" data-placement="top" title="' . $amount1[0] . '">' . number_price($amount1[1]) . '</span> -
-                <span data-toggle="tooltip" data-placement="top" title="' . $amount2[0] . '">' . number_price($amount2[1]) . '</span>
-                ' . (count($amount3) > 0 ? ' - <small data-toggle="tooltip" data-placement="top" title="' . $amount3[0] . '">' . number_price($amount3[1]) . '</small>' : '') . '
+                <span data-toggle="tooltip" data-placement="top" title="' . $amount2[0] . '">' . number_price($amount2[1]) . ' ' .
+        count($amount3) == 0 ? $percentage . '%' : '' . ' </span>
+                ' . (count($amount3) > 0 ? ' - <small data-toggle="tooltip" data-placement="top" title="' . $amount3[0] . '">' . number_price($amount3[1]) . ' ' . $percentage . '%</small>' : '') . '
             </h6>
         </div>
     </div>
