fbbc0555e9c8ee026dcf7119ed99b8418b80d1c7
Andres Lobaton <andrespipe021028@gmail.com>
2025-05-01 12:36:44 -0500

    feat: ajuste query para notificar subscripciones

--- app/models/MoneyLoanListener.php
+++ app/models/MoneyLoanListener.php
@@ -9,27 +9,25 @@ class MoneyLoanListener extends Orm
     public function getNotificationListeners()
     {
         $sql = "SELECT
-                mn.id,
-                mn.id_money_loan,
-                email as email_to,
-                username as username_to,
-                is_subscription,
-                total,
-                set_date as expiration_date,
-                mn.executed_at
-            FROM moneyloan_notifications mn
-            JOIN moneyloans m ON m.id_money_loan = mn.id_money_loan
-            WHERE is_active = true AND m.status = true  AND (
-                is_subscription = 0
-                and executed_at is null
-                OR executed_at <> set_date
-                and set_date = curdate()
-            ) AND (
-                is_subscription = 1
-                and executed_at is null
-                OR DATE_FORMAT(executed_at, '%m-%d') <> DATE_FORMAT(set_date, '%m-%d')
-                AND DATE_FORMAT(set_date, '%m-%d') = DATE_FORMAT(CURDATE(), '%m-%d')
-            )";
+            mn.id,
+            mn.id_money_loan,
+            email as email_to,
+            username as username_to,
+            is_subscription,
+            total,
+            set_date as expiration_date,
+            mn.executed_at
+        FROM moneyloan_notifications mn
+        JOIN moneyloans m ON m.id_money_loan = mn.id_money_loan
+        WHERE is_active = true AND m.status = true  AND (
+            is_subscription = 0
+            AND executed_at <> set_date
+            and set_date = curdate()
+        ) OR (
+            is_subscription = 1
+            AND executed_at <> set_date
+            AND DATE_FORMAT(set_date, '%d') = DATE_FORMAT(CURDATE(), '%d')
+        )";
         $this->querye($sql);
         $this->execute();
         return new JSON($this->fetchAll());
